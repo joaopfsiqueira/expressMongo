518b8c6b69f264718b61f9594edc32724628f67b
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-04-07 14:01:33 -0300

    fix: autorModels

--- src/models/Autor.js
+++ src/models/Autor.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const autorSchema = new mongoose.Schema({
     id: { type: String },  
     nome: { type: String, required: true},
-    nacionaliodade: { type: String},
+    nacionalidade: { type: String},
     },
     {
         versionKey: false,
