19fd8c311f8c7256f511dd2eb7f0ec6b53a39f85
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-04-07 01:14:35 -0300

    feat: tirando os métodos de id e atualizar do antigo app.js

--- src/app.js
+++ src/app.js
@@ -25,17 +25,6 @@ routes(app)
 // ]
 
 
-app.put('/livros/:id', (req, res) => {
-    let index = buscaLivro(req.params.id);
-    livros[index].titulo = req.body.titulo;
-    res.status(200).json(livros);
-})
-
-app.get('/livros/:id', (req, res) => {
-    let index = buscaLivro(req.params.id);
-    res.status(200).json(livros[index]);
-})
-
 app.delete('/livros/:id', (req, res) => {
     let {id} = req.params;
     let index = buscaLivro(id);
